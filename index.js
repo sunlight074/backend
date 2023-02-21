cbb8bc6782eaa0371bbb0edb961039dcba1c1999
worawut srimala <worawut48@hotmail.co.th>
2023-02-21 21:57:59 +0700

    post api

--- index.js
+++ index.js
@@ -16,15 +16,26 @@ const corsOption = {
     credentials : true
 }
 
+app.use(express.json());
+
 app.use(cors(corsOption))
 
-app.get('/',(_req, res)=>{
+app.get('/',(_req, res)=> { // get ใช้สำหรับดึงข้อมูล
     res.send('hello world')
 })
 
 app.get('/api/list-user',(_req, res)=>{
     res.send({result:userData})
 })
+
+app.post('/api/register',(req, res)=>{ //POST ใช้สำหรับสร้างข้อมูล
+    res.send({ result: req.body })
+})
+
+app.post('/api/login',(req, res)=>{ //POST ใช้สำหรับสร้างข้อมูล
+    res.send({ result: req.body })
+})
+
 
 app.listen(port, ()=>{
     console.log('listening in port ',port)
