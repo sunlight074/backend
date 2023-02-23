3c93887a8293e328e823bbf64c04dd7335c00fe1
worawut srimala <worawut48@hotmail.co.th>
2023-02-23 23:46:52 +0700

    add handlerLogin

--- controller/login/index.js
+++ controller/login/index.js
@@ -1,5 +1,20 @@
+const con = require('../../connection')
+
 const handlerLogin = (req , res) => {
-    res.send({result:'wanee'})
+    const {email , password } = req.body
+
+    try{
+        con.query(`SELECT * FROM USER WHERE email = '${email}' AND password = '${password}'`,
+        (err , result , _fields) => {
+            if(err) {
+                  return res.status(400).send();
+            }
+
+            return res.status(200).json({result : result})
+        })
+    }catch(err){
+        return res.status(500).send();
+    }
 }
 
 module.exports = handlerLogin
