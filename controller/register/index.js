4dd8cd40be9054095baa9cbf7b92c8c835bd49cc
worawut srimala <worawut48@hotmail.co.th>
2023-02-24 22:52:49 +0700

    condition

--- controller/register/index.js
+++ controller/register/index.js
@@ -4,14 +4,28 @@ const handlerRegister = (req , res) => {
     const {email , password , first_name , last_name} = req.body
 
     try{
-        con.query(`INSERT INTO USER (email ,password,first_name ,last_name ) VALUES ('${email}','${password}','${first_name}','${last_name}')`,
-        (err , _result , _fields) => {
+        con.query(`SELECT * FROM USER WHERE email = '${email}'`,
+        (err , result , _fields) => {
             if(err) {
                   return res.status(400).send();
             }
 
+            if(result.length === 1){ // แปลว่าไม่มีข้อมูล
+                return res.status(400).send();
+            }
+
+            con.query(`INSERT INTO USER (email ,password,first_name ,last_name ) VALUES ('${email}','${password}','${first_name}','${last_name}')`,
+            (err , _result , _fields) => {
+            if(err) {
+                  return res.status(400).send();
+            }
+            
             return res.status(201).json({message : 'successfully created'})
         })
+
+          
+        })
+        
     }catch(err){
         return res.status(500).send();
     }
