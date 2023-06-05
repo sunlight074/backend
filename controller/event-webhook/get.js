1749542b86496cbb19a50937652ad49308390320
worawut <worawut48@hotmail.co.th>
2023-06-05 22:25:53 +0700

    update

--- controller/event-webhook/get.js
+++ controller/event-webhook/get.js
@@ -1,12 +1,17 @@
 const connection = require("../../connection");
 
-const getEventWebhook = (_req, res) => {
-  try {
-    connection.query("SELECT * FROM EVENT_LOG", (error, result, _filds) => {
-      if (error) return res.status(400);
+const getEventWebhook = (req, res) => {
+  const findId = req.query.id ? `WHERE id = ${req.query.id}` : "";
 
-      return res.send({ result: result });
-    });
+  try {
+    connection.query(
+      `SELECT * FROM EVENT_LOG ${findId}`,
+      (error, result, _filds) => {
+        if (error) return res.status(400);
+
+        return res.send({ result: result });
+      }
+    );
   } catch (error) {
     return res.status(500).send();
   }
